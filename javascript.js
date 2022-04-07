1f2edbffd97247eefbf8d7957185235d6c7c265c
xzthan <nmaldonado2725@gmail.com>
2022-04-06 20:16:50 -0700

    scrollUp function

--- javascript.js
+++ javascript.js
@@ -132,3 +132,11 @@ var story = {
     },
 
 };
+
+
+mybutton = document.getElementById("buttons");
+
+function scrollUp() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
